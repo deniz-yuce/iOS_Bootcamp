f09246bdcca86b6da591cb626a7f876b04588e5f
denizyce <denizyce1004@gmail.com>
2022-06-02 16:44:41 +0300

    homepage

--- AkisYemek/AkisYemek/foodinfo-module/interactor/FoodInfoInteractor.swift
+++ AkisYemek/AkisYemek/foodinfo-module/interactor/FoodInfoInteractor.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 class FoodInfoInteractor : PresenterToInteractorFoodInfoProtocol {
-    func addFood(yemek_adi: String, yemek_fiyat: Int, yemek_siparis_adet: Int) {
-        print("Adding to cart : \(yemek_adi) - \(yemek_fiyat) - \(yemek_siparis_adet)")
+    func addFood(yemek_adi: String, yemek_fiyat: String, yemek_siparis_adet: String, yemek_resim_adi: String) {
+        print("Adding to cart : \(yemek_adi) - \(yemek_fiyat) - \(yemek_siparis_adet) - \(yemek_resim_adi)")
         //(yemek_resim_adi) göndercen
         //var ex = "Deniz"
         //print(ex.lowercased()) OLARAK GONDER + USER ID GÖNDERCEN
